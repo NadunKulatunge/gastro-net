9d923a0d5c85dca058a590f88c60c66e714bbea7
Nadun Kulatunge <35257469+NadunKulatunge@users.noreply.github.com>
2021-01-31 15:44:21 +0530

    Updated changes

--- app.py
+++ app.py
@@ -1,52 +1,51 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 26 00:40:50 2020
-@author: ASUS
-"""
+# app.py
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 
-import numpy as np
-import streamlit as st
-import tensorflow as tf
-from PIL import Image, ImageOps
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
 
-def import_and_predict(image_data, model):
-    
-        size = (75,75)    
-        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-        image = image.convert('RGB')
-        image = np.asarray(image)
-        image = (image.astype(np.float32) / 255.0)
+    # For debugging
+    print(f"got name {name}")
 
-        img_reshape = image[np.newaxis,...]
+    response = {}
 
-        prediction = model.predict(img_reshape)
-        
-        return prediction
-
-model = tf.keras.models.load_model('models/endoscopy_vgg16.h5')
-
-st.write("""
-         # Rock-Paper-Scissor Hand Sign Prediction
-         """
-         )
-
-st.write("This is a simple image classification web app to predict rock-paper-scissor hand sign")
-
-file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
-#
-if file is None:
-    st.text("You haven't uploaded an image file")
-else:
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-    prediction = import_and_predict(image, model)
-    
-    if np.argmax(prediction) == 0:
-        st.write("It is a paper!")
-    elif np.argmax(prediction) == 1:
-        st.write("It is a rock!")
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
     else:
-        st.write("It is a scissor!")
-    
-    st.text("Probability (0: Paper, 1: Rock, 2: Scissor)")
-    st.write(prediction)
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
