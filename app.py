01e24c2bdae467d6d16ba8aa03c234f035804ef3
Nadun Kulatunge <nadunku.16@cse.mrt.ac.lk>
2021-01-31 18:13:58 +0530

    Nuew

--- app.py
+++ app.py
@@ -1,51 +1,124 @@
-# app.py
-from flask import Flask, request, jsonify
+#Usage: python app.py
+import os
+ 
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from werkzeug.middleware.shared_data import SharedDataMiddleware
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from keras.models import Sequential, load_model
+import numpy as np
+import argparse
+import imutils
+import cv2
+import time
+import uuid
+import base64
+
+img_width, img_height = 224, 224
+model_path = './models/endoscopy_vgg16.h5'
+#model_weights_path = './models/weights.h5'
+model = load_model(model_path)
+#model.load_weights(model_weights_path)
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
+
+def get_as_base64(url):
+    return base64.b64encode(requests.get(url).content)
+
+def predict(file):
+    x = load_img(file, target_size=(img_width,img_height))
+    x = img_to_array(x)
+    x = np.expand_dims(x, axis=0)
+    array = model.predict(x)
+    result = array[0]
+    answer = np.argmax(result)
+    if answer == 0:
+        print("Label: Dyed Lifted Polyp")
+    elif answer == 1:
+	    print("Label: Dyed Resection Margin")
+    elif answer == 2:
+	    print("Label: Esophagitis")
+    elif answer == 3:
+	    print("Label: Normal Cecum")
+    elif answer == 4:
+	    print("Label: Normal Pylorus")
+    elif answer == 5:
+	    print("Label: Normal Z-Line")
+    elif answer == 6:
+	    print("Label: Polyps")
+    elif answer == 7:
+	    print("Label: Ulcerative Colitis")
+    return answer
+
+def my_random_string(string_length=10):
+    """Returns a random string of length string_length."""
+    random = str(uuid.uuid4()) # Convert UUID format to a Python string.
+    random = random.upper() # Make all characters uppercase.
+    random = random.replace("-","") # Remove the UUID '-'.
+    return random[0:string_length] # Return the random string.
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+@app.route("/")
+def template_test():
+    return render_template('template.html', label='', imagesource='../uploads/template.jpg')
 
-    # For debugging
-    print(f"got name {name}")
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        import time
+        start_time = time.time()
+        file = request.files['file']
 
-    response = {}
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            result = predict(file_path)
+            if result == 0:
+                label = 'Dyed Lifted Polyp'
+            elif result == 1:
+                label = 'Dyed Resection Margin'			
+            elif result == 2:
+                label = 'Esophagitis'
+            elif result == 3:
+                label = 'Normal Cecum'
+            elif result == 4:
+                label = 'Normal Pylorus'
+            elif result == 5:
+                label = 'Normal Z-Line'
+            elif result == 6:
+                label = 'Polyps'
+            elif result == 7:
+                label = 'Ulcerative Colitis'
+            print(result)
+            print(file_path)
+            filename = my_random_string(6) + filename
 
-    # Return the response in json format
-    return jsonify(response)
+            os.rename(file_path, os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print("--- %s seconds ---" % str (time.time() - start_time))
+            return render_template('template.html', label=label, imagesource='../uploads/' + filename)
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+from flask import send_from_directory
 
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
+
+if __name__ == "__main__":
+    app.debug=False
+    app.run(host='0.0.0.0', port=3000)
